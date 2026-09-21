d115bf01460f37175d8bc5a50e5ef6dcfe7f6a1e
GhoostVision <promptondrive@gmail.com>
2026-09-21 20:25:09 -0300

    aula 6

--- estudo.js
+++ estudo.js
@@ -1,5 +1,6 @@
-var nome = "Biel"
-var profisao = "Programador"
-var meses = "6"
+var nomeJogo = String(window.prompt(`Qual jogo voce quer buscar?`));
+var generoJogo = window.prompt(`Qual e o genero dele?`);
+var precoJogo = Number(window.prompt(`Qual preco voce gostaria de comprar?`));
+var lanacamentoJogo = window.prompt(`Quando foi lancado?`);
 
-console.log(`Bem vindo, ${nome}, sua proficao e ${profisao}, e vc trabla nela a ${meses} meses`)
+window.alert(`Seu jogo e ${nomeJogo.toUpperCase()}, ele possui ${nomeJogo.length} caracteres, o genero e ${generoJogo.toLowerCase()} o preco e de RS$ ${precoJogo.toFixed(2)}, lancado em ${lanacamentoJogo}`)
