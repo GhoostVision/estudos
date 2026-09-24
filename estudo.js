4be14cfdf96379ac1198ec913e51a8d1d124a677
GhoostVision <promptondrive@gmail.com>
2026-09-24 19:02:05 -0300

    aula 9 dom

--- estudo.js
+++ estudo.js
@@ -1,7 +1,1 @@
-var jogoNome = window.prompt(`Digite o nome do jogos`)
-var jogoIdade = Number(window.prompt(`Digite e a sua idade`))
-var jogoPreco = Number(window.prompt(`Digite o preco`))
-var exibirIdade = jogoIdade >= 18 ? `PODE JOGAR PELA IDADE` : `NAO PODE JOGAR PELA IDADE`
-var valorExebir = jogoPreco >= 100 ? `PRECO ALTO` : `PRECO BAIXO`
-
-window.alert(`JOGO: ${jogoNome.toUpperCase()}, SEU JOGO TEM:${jogoNome.length} CARACTRES, POSSUI O PRECO: R$ ${jogoPreco.toFixed(2).replace(".",",")}, CLASSIFICADO PRECO COMO:${valorExebir}, E VOCE ${exibirIdade}`)
+var vermelho = window.document.getElementById(`vermelho`).innerText = `Alterado no JS`
