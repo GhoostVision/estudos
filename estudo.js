608e284d20d9e32ec873c13793e7069e325d6078
GhoostVision <promptondrive@gmail.com>
2026-09-26 13:35:30 -0300

    aula 10 :0

--- estudo.js
+++ estudo.js
@@ -1,1 +1,15 @@
-var vermelho = window.document.getElementById(`vermelho`).innerText = `Alterado no JS`
+function evento(){
+    var vermelho = window.document.getElementById(`vermelho`)
+    vermelho.innerHTML = `Clicou`
+    vermelho.style.color = `green`
+}
+
+function passar(){
+    var meio = window.document.getElementById(`meio`)
+    meio.style.backgroundColor = `red`
+}
+
+function sair(){
+    var meio = window.document.getElementById(`meio`)
+    meio.style.backgroundColor = `blue`
+}
